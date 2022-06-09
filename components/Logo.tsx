21fa89a6a82800126cb942ea4040127f4760c155
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-08 23:18:53 -0700

    feat: add Logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface LogoProps {
+  className?: string
+};
+
+const Logo = ({ className }: LogoProps) => {
+  return (
+    <div className={className}>
+      <p>E</p>
+    </div>
+  );
+};
+
+const StyledLogo = styled(Logo)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: var(--fnt-regular);
+  color: var(--clr-gainsboro);
+  width: 40px;
+  height: 40px;
+  border: 1px solid white;
+`;
+
+export default StyledLogo;
