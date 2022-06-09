4867933fbae96ddb20c2f4f3a0c8995a75186438
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-09 12:58:45 -0700

    feat: add MenuContainer to NavBar

--- components/Nav/NavBar.tsx
+++ components/Nav/NavBar.tsx
@@ -1,20 +1,71 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+// Assets
+import { TextAnimateLi, useTextAnimate } from '../../assets/hooks/useTextAnimate';
+import viewport from '../../viewportSizes';
+
 // Components
 import StyledLogo from '../Logo';
 import StyledHamBtnContainer from './HamBtnCont';
 import StyledDropDownMenu from './DropDown';
 
-interface NavProps {
-  className?: string,
-  setHamIsActive: Function,
+
+interface MenuContainerProps {
+  readonly className?: string;
+  menuItems: string[];
+}
+
+const MenuContainer = ({ className, menuItems }: MenuContainerProps) => {
+  const data = useTextAnimate(menuItems.map((itemName) => {
+    return { name: itemName, isActive: true};
+  }));
+
+  interface itemProps {
+    name: string;
+    isActive: boolean;
+  }
+
+  return (
+    <ul className={className}>
+      {data.content.map((item: itemProps, index: number) => {
+        return (
+          <TextAnimateLi
+            key={index}
+            onMouseEnter={() => data.setOneActive(item.name)}
+            onMouseLeave={() => data.setAllActive()}
+            content={data.content}
+            index={index}
+          >
+            {item.name}
+          </TextAnimateLi>
+        );
+      })}
+    </ul>
+  );
 };
 
-const Nav = ({ className, setHamIsActive }: NavProps) => {
+const StyledMenuContainer = styled(MenuContainer)`
+  display: none;
+
+  @media (${viewport.lg}) {
+    display: flex;
+    gap: 60px;
+    color: var(--clr-gainsboro);
+  }
+`;
+
+interface NavProps {
+  className?: string;
+  setHamIsActive: Function;
+  menuItems: string[];
+};
+
+const Nav = ({ className, setHamIsActive, menuItems }: NavProps) => {
   return (
     <nav className={className}>
       <StyledLogo />
+      <StyledMenuContainer menuItems={menuItems} />
       <StyledHamBtnContainer setHamIsActive={setHamIsActive} />
     </nav>
   );
@@ -37,11 +88,17 @@ interface NavContProps {
 
 const NavCont = ({ className }: NavContProps) => {
   const [hamIsActive, setHamIsActive] = useState(false);
+  const [menuItems] = useState([
+    'work',
+    'about',
+    'contact',
+    'cv',
+  ]);
 
   return (
     <div className={className}>
-      <StyledNav setHamIsActive={setHamIsActive} />
-      <StyledDropDownMenu hamIsActive={hamIsActive} />
+      <StyledNav setHamIsActive={setHamIsActive} menuItems={menuItems} />
+      <StyledDropDownMenu hamIsActive={hamIsActive} menuItems={menuItems}/>
     </div>
   );
 };
