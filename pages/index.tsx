499bd650c24a5734008a39d5002f3054922a62e8
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-08 23:26:13 -0700

    refactor: rename Container component to StyledContainer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,19 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 // Components
-import Container from '../components/Container';
+import StyledContainer from '../components/Container';
 import StyledNavCont from '../components/Nav/NavBar';
 
 const Home: NextPage = () => {
   return (
-    <Container>
+    <StyledContainer>
       <Head>
         <title>Edgar Carrillo | Full Stack Developer</title>
         <meta name="Personal Portfolio" content="#" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <StyledNavCont />
-    </Container>
+    </StyledContainer>
   )
 }
 
