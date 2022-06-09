7c65535d90c94dd01e63d1057639e390e9340cbb
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-08 22:36:16 -0700

    feat: add viewport sizes for consistent responsive design

---
+++ viewportSizes.js
@@ -0,0 +1,9 @@
+const viewport = {
+  sm: 'min-width: 640px',
+  md: 'min-width: 768px',
+  lg: 'min-width: 1024px',
+  xl: 'min-width: 1280px',
+  '2xl': 'min-width: 1536px',
+};
+
+export default viewport;
