ae0ba04312a472122277cbf4cb82adcaf8a0280c
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-12 16:40:25 -0700

    feat: create validation handlers for form

---
+++ components/Form/formFuncs.tsx
@@ -0,0 +1,35 @@
+const nameHandler = (name: string) => {
+  if (name.length > 0 && name.length <= 20) return true;
+  console.log(name);
+
+  return false;
+};
+
+const subjectHandler = (subject: string) => {
+  if (subject.length > 0 && subject.length <= 25) return true;
+
+  return false;
+};
+
+const emailHandler = (subject: string, builtInValidation: boolean) => {
+  if (builtInValidation) {
+    if (subject.length > 0 && subject.length <= 30) return true;
+  }
+
+  return false;
+};
+
+const messageHandler = (subject: string, builtInValidation: boolean) => {
+  if (builtInValidation) {
+    if (subject.length > 0 && subject.length <= 500) return true;
+  }
+
+  return false;
+};
+
+export {
+  nameHandler,
+  subjectHandler,
+  emailHandler,
+  messageHandler,
+};
