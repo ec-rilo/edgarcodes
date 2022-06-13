8fc33093e9e329078a7c769f592dc55855d75273
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-12 17:50:37 -0700

    feat: add Submit btn

---
+++ components/Form/SubmitBtn.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const StyledSubmitBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 150px;
+  height: 60px;
+
+  background: transparent;
+  border: 1px solid var(--clr-gainsboro);
+  color: var(--clr-gainsboro);
+  font-size: 1rem;
+  cursor: pointer;
+`;
+
+export default StyledSubmitBtn;
