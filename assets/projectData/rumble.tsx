5e2512895c231f1957d17ba1b56e3771ba2c37c3
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-13 14:42:40 -0700

    feat: add rumble data

---
+++ assets/projectData/rumble.tsx
@@ -0,0 +1,153 @@
+import imgSrc from '../../public/images/women-walking-img.jpeg';
+import heroImg from '../../public/images/projects/rumble/rumble-hero-image.png';
+
+const skillsArr = [
+  'placeholder',
+  'placeholder',
+  'placeholder',
+  'placeholder',
+  'placeholder',
+];
+
+const skillsParagraphs = [
+  `I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.`
+];
+
+const problemParagraphs = [
+  `I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.`
+];
+
+const imgsArr2 = [
+  { src: imgSrc, alt: "#"},
+];
+
+const imgsArr = [
+  { src: imgSrc, alt: "#"},
+  { src: imgSrc, alt: "#"},
+  { src: imgSrc, alt: "#"},
+];
+
+const hero = {
+
+  title: 'Rumble',
+  subtitle: 'Food ordering app, done Tinder style',
+  paragraphsArr: [
+    `
+    I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.
+    `
+  ],
+  repo: 'https://github.com/dennisrcao/Rumble',
+  imgSrc: heroImg,
+
+};
+
+const purpose = {
+
+  paragraphsArr: [
+    `
+    I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.
+    `
+  ],
+  imgSrc: heroImg,
+  alt: '',
+
+};
+
+const skills = {
+
+  skillsArr: [
+    'placeholder',
+    'placeholder',
+    'placeholder',
+    'placeholder',
+    'placeholder',
+    'placeholder',
+    'placeholder',
+    'placeholder',
+  ],
+  paragraphsArr: [
+    `
+    I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.
+    `
+  ],
+
+};
+
+const problem = {
+
+  paragraphsArr: [
+    `
+    I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.
+    `
+  ],
+
+};
+
+const pictures = {
+
+  arr: [
+    { src: heroImg, alt: "#"},
+    { src: heroImg, alt: "#"},
+    { src: heroImg, alt: "#"},
+  ]
+
+};
+
+const pictures2 = {
+
+  arr: [
+    { src: heroImg, alt: "#"},
+  ]
+
+};
+
+const whatsNext = {
+
+  paragraphsArr: [
+    `
+    I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.
+    `
+  ],
+
+};
+
+const lessons = {
+
+  paragraphsArr: [
+    `
+    I've taken courses that had an emphasis on understanding web development technologies as well as the basics of computer science concepts. After having pair programmed with many different people and applied the web development concepts into many projects you can now see my skills reflected on all new projects I've created.
+    `
+  ],
+
+};
+
+const altProjects = [
+
+  {
+    title: 'Rumble',
+    subTitle: 'Web Development',
+    src: heroImg,
+    alt: '',
+  },
+  {
+    title: 'Rumble',
+    subTitle: 'Web Development',
+    src: heroImg,
+    alt: '',
+  },
+
+];
+
+const rumbleData = {
+  hero,
+  purpose,
+  skills,
+  problem,
+  pictures,
+  lessons,
+  whatsNext,
+  altProjects,
+  pictures2,
+};
+
+export default rumbleData;
