515ba35caa7cc426d97482438f86ed5d55f803bb
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-28 17:34:44 -0700

    refactor: update category for each project

--- assets/homeData/projectsData.tsx
+++ assets/homeData/projectsData.tsx
@@ -5,7 +5,7 @@ import rumbleSrc from '../../public/images/projects/rumble/rumble-desktop-mobile
 const projectsArr = [
   {
     title: "Rumble",
-    category: "Web Development",
+    category: "Full Stack Engineer - UI Owner",
     textContent:
       "A food ordering app, done tinder-style, that allows users to swipe right or left on restaurants, alone or in a group. View dedicated Restaurant pages along with the menu, and much more!",
     src: rumbleSrc,
@@ -15,7 +15,7 @@ const projectsArr = [
   },
   {
     title: "Fun with GPT-3",
-    category: "Web Development",
+    category: "Full Stack Software Engineer",
     textContent:
       "Play around with AI and ask it anything! Whatever you ask will be projected LIVE to any user using the application! Your results will vary based on the engine you choose when asking a question.",
     src: imgSrc,
@@ -25,7 +25,7 @@ const projectsArr = [
   },
   {
     title: "Atelier",
-    category: "Web Development",
+    category: "Frontend Engineer",
     textContent:
       "Through timeblocking, using trello with my team, and following AGILE best practices - I was able to create a product details page with a RESTful Back End that surpassed client expectations.",
     src: imgSrc,
@@ -35,7 +35,7 @@ const projectsArr = [
   },
   {
     title: "Threads",
-    category: "Web Development",
+    category: "Backend Software Engineer",
     textContent:
       "A Back-End system redesign for the products module of a legacy e-commerce API with 5+ million database entries with average query times of 1000ms+ per request. Converting the Monolithic service into dedicated micro-services.",
     src: imgSrc,
@@ -45,7 +45,7 @@ const projectsArr = [
   },
   {
     title: "Rumble Remake",
-    category: "Web Development",
+    category: "Full Stack Engineer",
     textContent:
       "A full remake of Rumble that takes the foundation of the original product and extends it with Restaurants near the user, actual group swipe mode, and much more!",
     src: imgSrc,
